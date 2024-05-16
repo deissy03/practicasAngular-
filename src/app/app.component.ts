041179ae8b11e99e9f49bf311a36f7dce842f6e7
deisy03 <desiyesquivia03@gmail.com>
2024-05-16 15:40:33 -0600

    creando rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {NavegacionComponent}from "./components/navegacion/navegacion.component";
 import {InicioComponent}from "./components/inicio/inicio.component"
+import {ContactosComponent}from "./components/contactos/contactos.component"
 import {ProductosComponent}from "./components/productos/productos.component"
 import {NosotrosComponent}from "./components/nosotros/nosotros.component"
 import {ServiciosComponent}from "./components/servicios/servicios.component"
@@ -14,7 +15,7 @@ import {NoEncontradoComponent}from "./components/no-encontrado/no-encontrado.com
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,NavegacionComponent,InicioComponent,ProductosComponent,NosotrosComponent,ServiciosComponent,
+  imports: [RouterOutlet,CommonModule,NavegacionComponent,InicioComponent,ContactosComponent,ProductosComponent,NosotrosComponent,ServiciosComponent,
     RegistrosComponent,IniciarSesionComponent,PrivadoComponent,NoEncontradoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {InicioComponent}from "./components/inicio/inicio.component"
 import {ProductosComponent}from "./components/productos/productos.component"
 import {NosotrosComponent}from "./components/nosotros/nosotros.component"
 import {ServiciosComponent}from "./components/servicios/servicios.component"
+import {ContactosComponent}from "./components/contactos/contactos.component"
 import {RegistrosComponent}from "./components/registros/registros.component"
 import {IniciarSesionComponent}from "./components/iniciar-sesion/iniciar-sesion.component"
 import { PrivadoComponent } from "./components/privado/privado.component"
-//import {NoEncontradoComponent}from "./components/no-encontrado/no-encontrado.component"
+import {NoEncontradoComponent}from "./components/no-encontrado/no-encontrado.component"
+
 
 
 export const routes: Routes = [
@@ -14,11 +16,12 @@ export const routes: Routes = [
     {path:"productos", component:ProductosComponent},
     {path:"nosotros", component:NosotrosComponent},
     {path:"servicios", component:ServiciosComponent},
+    {path:"contactos", component:ContactosComponent},
     {path:"registros", component:RegistrosComponent},
     {path:"iniciar sesion", component:IniciarSesionComponent},
     {path:"privado", component:PrivadoComponent},
-    //{path:"**", component:NoEncontradoComponent, title:"404"},
-    //{path:"",redirectTo:"inicio", pathMatch:"full" }, 
+    {path:"**", component:NoEncontradoComponent, title:"404"},
+    {path:"",redirectTo:"inicio", pathMatch:"full" }, 
 
 ];
 
