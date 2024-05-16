1e3825531ecb2d0057b4d65d7ddfb0ed660110e1
deisy03 <desiyesquivia03@gmail.com>
2024-05-16 15:19:46 -0600

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NavegacionComponent}from "./components/navegacion/navegacion.component";
+import {InicioComponent}from "./components/inicio/inicio.component"
+import {ProductosComponent}from "./components/productos/productos.component"
+import {NosotrosComponent}from "./components/nosotros/nosotros.component"
+import {ServiciosComponent}from "./components/servicios/servicios.component"
+import {RegistrosComponent}from "./components/registros/registros.component"
+import {IniciarSesionComponent}from "./components/iniciar-sesion/iniciar-sesion.component"
+import { PrivadoComponent } from "./components/privado/privado.component"
+import {NoEncontradoComponent}from "./components/no-encontrado/no-encontrado.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,NavegacionComponent,InicioComponent,ProductosComponent,NosotrosComponent,ServiciosComponent,
+    RegistrosComponent,IniciarSesionComponent,PrivadoComponent,NoEncontradoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular-proyect';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {InicioComponent}from "./components/inicio/inicio.component"
+import {ProductosComponent}from "./components/productos/productos.component"
+import {NosotrosComponent}from "./components/nosotros/nosotros.component"
+import {ServiciosComponent}from "./components/servicios/servicios.component"
+import {RegistrosComponent}from "./components/registros/registros.component"
+import {IniciarSesionComponent}from "./components/iniciar-sesion/iniciar-sesion.component"
+import { PrivadoComponent } from "./components/privado/privado.component"
+//import {NoEncontradoComponent}from "./components/no-encontrado/no-encontrado.component"
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:"inicio", component: InicioComponent, title:"Inicio"},
+    {path:"productos", component:ProductosComponent},
+    {path:"nosotros", component:NosotrosComponent},
+    {path:"servicios", component:ServiciosComponent},
+    {path:"registros", component:RegistrosComponent},
+    {path:"iniciar sesion", component:IniciarSesionComponent},
+    {path:"privado", component:PrivadoComponent},
+    //{path:"**", component:NoEncontradoComponent, title:"404"},
+    //{path:"",redirectTo:"inicio", pathMatch:"full" }, 
+
+];
+
